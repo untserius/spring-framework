8b0cd05b760d7f661c7cd3866266bc2378abf51c
Sudhir Das <sudhirdas1999@gmail.com>
2024-04-09 16:43:48 +0530

    singleton vs prototype

---
+++ Spring Tool Suite Projects/project/src/main/java/com/firstspring/project/App.java
@@ -0,0 +1,26 @@
+package com.firstspring.project;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        // Loads the context from XML files located in the classpath "spring.xml".
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        // Retrieves the bean named "Serius" from the Spring application context.
+        // The getBean method returns an object of type Object, so you need to cast it to object type "Serius".
+        Serius s = (Serius) context.getBean("Serius");
+        s.age = 10;
+        s.code();
+        System.out.println(s.age);
+
+        Serius s1 = (Serius) context.getBean("Serius");
+        s1.code();
+        System.out.println(s1.age);
+
+    }
+}
---
+++ Spring Tool Suite Projects/project/src/main/java/com/firstspring/project/Serius.java
@@ -0,0 +1,15 @@
+package com.firstspring.project;
+
+public class Serius {
+
+	int age;
+
+	Serius(){
+		System.out.println("Object is getting created...");
+	}
+
+	public void code() {
+		System.out.println("I am coding...");
+	}
+
+}
